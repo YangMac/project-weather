f5f1892df24fd54a686c0bf6851a0f2c7fe6ec11
MacYang <yinnvc@gmail.com>
2019-01-11 23:59:11 +0800

    get from weather API

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,13 @@
-Page({})
+Page({
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '昆明市'
+      },
+      success: res => {
+        console.log(res)
+      }
+    })
+  }
+})
