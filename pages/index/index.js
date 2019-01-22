1c3f47c7be38f9550617cb2dd0e84f6800f3e920
MacYang <yinnvc@gmail.com>
2019-01-22 20:15:13 +0800

    Update: show today info and arrow icon

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,7 +21,9 @@ Page({
     nowTemp: '',
     nowWeather: '',
     nowWeatherBackground: '',
-    hourlyWeather: []
+    hourlyWeather: [],
+    todayDate: '',
+    todayTemp: ''
   },
   
   onPullDownRefresh() {
@@ -45,6 +47,7 @@ Page({
         let result = res.data.result
         this.setNow(result)
         this.setHourlyWeather(result)
+        this.setToday(result)
       },
       complete: () => {
         callback && callback()
@@ -80,6 +83,13 @@ Page({
     hourlyWeather[0].time = '现在'
     this.setData({
       hourlyWeather: hourlyWeather
+    })
+  },
+  setToday(result) {
+    let date = new Date()
+    this.setData({
+      todayTemp: `${result.today.minTemp}° - ${result.today.maxTemp}`,
+      todayDate: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} 今天`
     })
   }
 })
